67992ab46a3ff028e794104bd55c997cf24bef57
Katie Cann <katietaylorcann@gmail.com>
2015-04-11 18:15:04 -0400

    Completed assignment

--- truncate.js
+++ truncate.js
@@ -4,14 +4,30 @@
 // longText (a String with several words in it)
 // numWords (an Integer that sets the number of words you want in the returned text)
 
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
+function truncateWords(longText, numWords){
+	var longTextArray = longText.split(' ');  // Use the split() function to split the String into an Array
+	console.log(longTextArray);
+	
+	var length = longTextArray.length;  // Use the length attribute to find the number of words in the Array
+	console.log(length);
+	
+	var shortenedArray = longTextArray.splice(0, numWords);  // Determine how many words should be removed from the String. Remove those words from the Array
+
+	console.log(shortenedArray);
+	
+	var truncatedString = shortenedArray.join(' ') + ('...');  // Use the join() function to convert the Array back into a String.  Add an additional String item to the Array to put an ellipses in: "..."
+	
+	return truncatedString;  // Return the truncated String from the Function
+}
+
+numWords = (13);
+var longText = "Much like the fortified wine that gives Marsala its name this robust shade incorporates the richness of a fulfilling meal while its grounding red-brown roots point to a sophisticated earthiness.";
+console.log(truncatedString);
+
+
+
+
+
 
 // This portion of the script is meant to call and display the result of your Function.
 // You do not need to change the following lines, but you may change them if you 
